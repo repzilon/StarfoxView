3efb2e63018184b35dda3c6fdadd37275e1de795
repzilon <6383002+repzilon@users.noreply.github.com>
2025-06-23 17:49:52 -0400

    Use Meiryo as compatible font instead of Segoe UI
    Meiryo is a font for Japanese having Latin, Greek, and Cyrillic too that also debuted with Windows Vista. The non Japanese characters look like Verdana.

--- StarFoxMapVisualizer/Controls2/CommunicationMessageControl.xaml.cs
+++ StarFoxMapVisualizer/Controls2/CommunicationMessageControl.xaml.cs
@@ -5,88 +5,86 @@ using System.Windows.Media;
 
 namespace StarFoxMapVisualizer.Controls2
 {
-    /// <summary>
-    /// Interaction logic for CommunicationMessageControl.xaml
-    /// </summary>
-    public partial class CommunicationMessageControl : ContentControl, INotifyPropertyChanged
-    {
-        public Rect ImageRect { get; set; } = new Rect(0,0,31,39);
-        public Visibility MugshotVisibility { get; set; } = Visibility.Collapsed;
-        public FontFamily GameFont { get; set; }
+	/// <summary>
+	/// Interaction logic for CommunicationMessageControl.xaml
+	/// </summary>
+	public partial class CommunicationMessageControl : ContentControl, INotifyPropertyChanged
+	{
+		public Rect ImageRect { get; set; } = new Rect(0, 0, 31, 39);
+		public Visibility MugshotVisibility { get; set; } = Visibility.Collapsed;
+		public FontFamily GameFont { get; set; }
 
-        public enum Characters
-        {
-            FOX,
-            FALCON,
-            RABBIT,
-            FROG,
-            PEPPER,
-            ANDROSS,
-            BETA_SLIPPY
-        }
-        public static Characters MapSpeakerToCharacter(string Speaker)
-        {
-            if (Speaker == "fox" || Speaker == "fox3") {
-                return Characters.FOX;
-            } else if (Speaker == "falcon" || Speaker == "falcon3") {
-                return Characters.FALCON;
-            } else if (Speaker == "rabbit" || Speaker == "rabbit3") {
-                return Characters.RABBIT;
-            } else if (Speaker == "frog" || Speaker == "frog3") {
-                return Characters.FROG;
-            } else if (Speaker == "andross" || Speaker == "andross3") {
-                return Characters.ANDROSS;
-            } else if (Speaker == "pepper" || Speaker == "pepper3") {
-                return Characters.PEPPER;
-            } else {
-                return Characters.BETA_SLIPPY;
-            }
-        }
+		public enum Characters
+		{
+			FOX,
+			FALCON,
+			RABBIT,
+			FROG,
+			PEPPER,
+			ANDROSS,
+			BETA_SLIPPY
+		}
 
-        public CommunicationMessageControl()
-        {
-            InitializeComponent();
-            SetCompatibleFonts();
-        }
+		public static Characters MapSpeakerToCharacter(string Speaker)
+		{
+			if (Speaker == "fox" || Speaker == "fox3") {
+				return Characters.FOX;
+			} else if (Speaker == "falcon" || Speaker == "falcon3") {
+				return Characters.FALCON;
+			} else if (Speaker == "rabbit" || Speaker == "rabbit3") {
+				return Characters.RABBIT;
+			} else if (Speaker == "frog" || Speaker == "frog3") {
+				return Characters.FROG;
+			} else if (Speaker == "andross" || Speaker == "andross3") {
+				return Characters.ANDROSS;
+			} else if (Speaker == "pepper" || Speaker == "pepper3") {
+				return Characters.PEPPER;
+			} else {
+				return Characters.BETA_SLIPPY;
+			}
+		}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+		public CommunicationMessageControl()
+		{
+			InitializeComponent();
+			SetCompatibleFonts();
+		}
 
-        public void DrawMugshot(Characters Character, int Frame = 0)
-        {
-            int baseX = 0;
-            int baseY = 0;
-            int charWidth = 31;
-            int charHeight = 39;// FOX FRAME 1
-            switch (Character)
-            {
-                case Characters.FOX: break;
-                case Characters.FALCON: baseX = (charWidth * 2) + 2; break;
-                case Characters.RABBIT: baseY = charHeight + 1; break;
-                case Characters.FROG: baseX = (charWidth * 2) + 2; baseY = charHeight + 1; break;
-                default:
-                    ImageMissing();
-                    return;
-            }
+		public event PropertyChangedEventHandler PropertyChanged;
 
-            MugshotVisibility = Visibility.Visible;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MugshotVisibility)));
+		public void DrawMugshot(Characters Character, int Frame = 0)
+		{
+			int baseX = 0;
+			int baseY = 0;
+			const int charWidth = 31;
+			const int charHeight = 39;// FOX FRAME 1
+			switch (Character) {
+				case Characters.FOX: break;
+				case Characters.FALCON: baseX = (charWidth * 2) + 2; break;
+				case Characters.RABBIT: baseY = charHeight + 1; break;
+				case Characters.FROG: baseX = (charWidth * 2) + 2; baseY = charHeight + 1; break;
+				default:
+					ImageMissing();
+					return;
+			}
 
-            ImageRect = new Rect(baseX + Frame * (charWidth + 1),baseY,charWidth,charHeight);
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImageRect)));
-        }
+			MugshotVisibility = Visibility.Visible;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MugshotVisibility)));
 
-        public void SetCompatibleFonts(bool Compatible = true)
-        {
-            if (Compatible)
-                GameFont = new FontFamily("Segoe UI");
-            else GameFont = FindResource("SFFont") as FontFamily;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GameFont)));
-        }
+			ImageRect = new Rect(baseX + Frame * (charWidth + 1), baseY, charWidth, charHeight);
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImageRect)));
+		}
 
-        private void ImageMissing()
-        {
-            MugshotVisibility = Visibility.Hidden;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MugshotVisibility)));
-        }
-    }
+		public void SetCompatibleFonts(bool Compatible = true)
+		{
+			GameFont = Compatible ? new FontFamily("Meiryo") : FindResource("SFFont") as FontFamily;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GameFont)));
+		}
+
+		private void ImageMissing()
+		{
+			MugshotVisibility = Visibility.Hidden;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MugshotVisibility)));
+		}
+	}
 }
