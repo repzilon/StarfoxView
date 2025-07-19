2f07e10841cdee7e8d15741a65bae189a7eb8558
repzilon <6383002+repzilon@users.noreply.github.com>
2025-07-19 17:44:17 -0400

    Better JSON serialization of maps
    No more opaque byte array. System.Text.Json needs extra attributes for polymorphic serialization of MAPEvents.

--- StarFox.Interop/MAP/EVT/MAPEvent.cs
+++ StarFox.Interop/MAP/EVT/MAPEvent.cs
@@ -10,272 +10,293 @@ using StarFox.Interop.ASM.TYP.STRUCT;
 
 namespace StarFox.Interop.MAP.EVT
 {
-    /// <summary>
-    /// Represents an unknown map event
-    /// </summary>
-    public class MAPUnknownEvent : MAPEvent
-    {
-        public ASMMacroInvokeParameter[] Parameters { get; protected set; }
-        protected override string[] CompatibleMacros { get; } =
-        {
+	/// <summary>
+	/// Represents an unknown map event
+	/// </summary>
+	public class MAPUnknownEvent : MAPEvent
+	{
+		public ASMMacroInvokeParameter[] Parameters { get; protected set; }
+		protected override string[] CompatibleMacros { get; } =
+		{
 
-        };
-        public MAPUnknownEvent() : base()
-        {
+		};
+		public MAPUnknownEvent() : base()
+		{
 
-        }
-        public MAPUnknownEvent(ASMLine Line) : base(Line)
-        {
+		}
+		public MAPUnknownEvent(ASMLine Line) : base(Line)
+		{
 
-        }
+		}
 
-        protected override void Parse(ASMLine Line)
-        {
-            Callsite = Line;
-            EventName = Line.StructureAsMacroInvokeStructure.MacroReference.Name;
-            Parameters = Line.StructureAsMacroInvokeStructure.Parameters;
-        }
-    }
-    /// <summary>
-    /// Translated CtrlVar codes from Starfox MAPMACS.INC
-    /// </summary>
-    public enum MAPCtrlVars : short
-    {
-        None = -1,
-        ctrlmapobj = 0,
-        ctrlend = 2,
-        ctrlloop = 4,
-        ctrlmapdeb = 6,
-        ctrlmapnop = 8,
-        ctrlmapmother = 10,
-        ctrlmapremove = 12,
-        ctrlsetstage = 14,
-        ctrlsetbg = 16,
-        ctrlmapwait = 18,
-        ctrlsetbgm = 20,
-        ctrlnodots = 22,
-        ctrlgnddots = 24,
-        ctrlspacedust = 26,
-        ctrlsetothmus = 28,
-        ctrlvofson = 30,
-        ctrlvofsoff = 32,
-        ctrlhofson = 34,
-        ctrlhofsoff = 36,
-        ctrlmapobjzrot = 38,
-        ctrlmapjsr = 40,
-        ctrlmaprts = 42,
-        ctrlmapif = 44,
-        ctrlmapgoto = 46,
-        ctrlsetxrot = 48,
-        ctrlsetyrot = 50,
-        ctrlsetzrot = 52,
-        ctrlsetalvarb = 54,
-        ctrlsetalvarw = 56,
-        ctrlsetalvarl = 58,
-        ctrlsetalxvarb = 60,
-        ctrlsetalxvarw = 62,
-        ctrlsetalxvarl = 64,
-        ctrlsetfadeup = 66,
-        ctrlsetfadedown = 68,
-        ctrlsetalvarptrb = 70,
-        ctrlsetalvarptrw = 72,
-        ctrlsetvarobj = 74,
-        ctrlmapwaitfade = 76,
-        ctrlsetqfadeup = 78,
-        ctrlsetqfadedown = 80,
-        ctrlscreenoff = 82,
-        ctrlscreenon = 84,
-        ctrlzrotoff = 86,
-        ctrlzroton = 88,
-        ctrlmapspecial = 90,
-        ctrlsetvarb = 92,
-        ctrlsetvarw = 94,
-        ctrlsetvarl = 96,
-        ctrlsetbgslow = 98,
-        ctrlwaitsetbg = 100,
-        ctrlsetbginfo = 102,
-        ctrladdalvarptrb = 104,
-        ctrladdalvarptrw = 106,
-        ctrlfadetosea = 108,
-        ctrlfadetoground = 110,
-        ctrlmapqobj = 112,
-        ctrlmapobj8 = 114,
-        ctrlmapdobj = 116,
-        ctrlmapqobj2 = 118,
-        ctrl65816 = 120,
-        ctrlmapcodejsl = 122,
-        ctrlmapjmpvarless = 124,
-        ctrlmapjmpvarmore = 126,
-        ctrlmapjmpvareq = 128,
-        ctrlsendmessage = 130,
-        ctrlmapcspecial = 132,
-        ctrlnobj = 134,
-        ctrlmqnobj = 136,
-        ctrlmapwait2 = 138,
-        ctrlmapsetpath = 140,
-    }
-    /// <summary>
-    /// A base-class for all events in a map script
-    /// </summary>
-    [Serializable]
-    public abstract class MAPEvent
-    {
-        /// <summary>
-        /// The time in the level script this event appears at
-        /// </summary>
-        public int LevelDelay { get; set; }
-        public MAPCtrlVars CtrlOptCode = MAPCtrlVars.None;
-        public virtual string EventName { get; set; }
-        [JsonIgnore]
-        public ASMLine Callsite { get; set; }
-        /// <summary>
-        /// Overriden in inheritors -- the list of macros that are compatible with this type of MAPEvent
-        /// </summary>
-        protected abstract string[] CompatibleMacros { get; }
-        public MAPEvent()
-        {
+		protected override void Parse(ASMLine Line)
+		{
+			Callsite = Line;
+			EventName = Line.StructureAsMacroInvokeStructure.MacroReference.Name;
+			Parameters = Line.StructureAsMacroInvokeStructure.Parameters;
+		}
+	}
 
-        }
-        /// <summary>
-        /// Base constuctor for all map events
-        /// </summary>
-        /// <param name="callsite"></param>
-        public MAPEvent(ASMLine callsite) : this()
-        {
-            Parse(callsite);
-        }
-        /// <summary>
-        /// Checks if the given <see cref="ASMLine"/> contains a macro invokation
-        /// that is compatible with this type of <see cref="MAPEvent"/>
-        /// </summary>
-        /// <param name="Line"></param>
-        /// <returns></returns>
-        internal bool IsCompatible(ASMLine Line)
-        {
-            if (!Line.HasStructureApplied) return false;
-            if (!(Line.Structure is ASMMacroInvokeLineStructure)) return false;
-            var structure = Line.StructureAsMacroInvokeStructure;
-            return CompatibleMacros.Contains(structure.MacroReference.Name.ToLower());
-        }
-        /// <summary>
-        /// Tries to parse a new map event out of the given macro invoke expression
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="Callsite"></param>
-        /// <param name="Result"></param>
-        /// <returns></returns>
-        internal static bool TryParse<T>(ASMLine Callsite, out T Result) where T : MAPEvent, new()
-        {
-            var t = new T();
-            Result = t;
-            if (t.IsCompatible(Callsite))
-            {
-                t.Parse(Callsite);
-                return true;
-            }
-            return false;
-        }
-        protected abstract void Parse(ASMLine Line);
-        /// <summary>
-        /// Basic <see cref="int.TryParse(string?, out int)"/> or returns <see langword="default"/> (which is 0)
-        /// </summary>
-        /// <param name="content"></param>
-        /// <returns></returns>
-        protected int TryParseOrDefault(string content)
-        {
-            if (string.IsNullOrEmpty(content)) return 0;
-            if (int.TryParse(content, out int result)) { return result; }
-            return 0;
-        }
-    }
+	/// <summary>
+	/// Translated CtrlVar codes from Starfox MAPMACS.INC
+	/// </summary>
+	public enum MAPCtrlVars : short
+	{
+		None = -1,
+		ctrlmapobj = 0,
+		ctrlend = 2,
+		ctrlloop = 4,
+		ctrlmapdeb = 6,
+		ctrlmapnop = 8,
+		ctrlmapmother = 10,
+		ctrlmapremove = 12,
+		ctrlsetstage = 14,
+		ctrlsetbg = 16,
+		ctrlmapwait = 18,
+		ctrlsetbgm = 20,
+		ctrlnodots = 22,
+		ctrlgnddots = 24,
+		ctrlspacedust = 26,
+		ctrlsetothmus = 28,
+		ctrlvofson = 30,
+		ctrlvofsoff = 32,
+		ctrlhofson = 34,
+		ctrlhofsoff = 36,
+		ctrlmapobjzrot = 38,
+		ctrlmapjsr = 40,
+		ctrlmaprts = 42,
+		ctrlmapif = 44,
+		ctrlmapgoto = 46,
+		ctrlsetxrot = 48,
+		ctrlsetyrot = 50,
+		ctrlsetzrot = 52,
+		ctrlsetalvarb = 54,
+		ctrlsetalvarw = 56,
+		ctrlsetalvarl = 58,
+		ctrlsetalxvarb = 60,
+		ctrlsetalxvarw = 62,
+		ctrlsetalxvarl = 64,
+		ctrlsetfadeup = 66,
+		ctrlsetfadedown = 68,
+		ctrlsetalvarptrb = 70,
+		ctrlsetalvarptrw = 72,
+		ctrlsetvarobj = 74,
+		ctrlmapwaitfade = 76,
+		ctrlsetqfadeup = 78,
+		ctrlsetqfadedown = 80,
+		ctrlscreenoff = 82,
+		ctrlscreenon = 84,
+		ctrlzrotoff = 86,
+		ctrlzroton = 88,
+		ctrlmapspecial = 90,
+		ctrlsetvarb = 92,
+		ctrlsetvarw = 94,
+		ctrlsetvarl = 96,
+		ctrlsetbgslow = 98,
+		ctrlwaitsetbg = 100,
+		ctrlsetbginfo = 102,
+		ctrladdalvarptrb = 104,
+		ctrladdalvarptrw = 106,
+		ctrlfadetosea = 108,
+		ctrlfadetoground = 110,
+		ctrlmapqobj = 112,
+		ctrlmapobj8 = 114,
+		ctrlmapdobj = 116,
+		ctrlmapqobj2 = 118,
+		ctrl65816 = 120,
+		ctrlmapcodejsl = 122,
+		ctrlmapjmpvarless = 124,
+		ctrlmapjmpvarmore = 126,
+		ctrlmapjmpvareq = 128,
+		ctrlsendmessage = 130,
+		ctrlmapcspecial = 132,
+		ctrlnobj = 134,
+		ctrlmqnobj = 136,
+		ctrlmapwait2 = 138,
+		ctrlmapsetpath = 140,
+	}
 
-    /// <summary>
-    /// Base interface for all MapEventComponents
-    /// <para/><see cref="IMAPDelayEvent"/> etc.
-    /// </summary>
-    public interface IMAPEventComponent { }
+	/// <summary>
+	/// A base-class for all events in a map script
+	/// </summary>
+#if NET48 || NETSTANDARD2_0 || NET6_0
+    [JsonDerivedType(typeof(MAPAlVarEvent), nameof(MAPAlVarEvent))]
+    [JsonDerivedType(typeof(MAPEndEvent), nameof(MAPEndEvent))]
+    [JsonDerivedType(typeof(MAPUnknownEvent), nameof(MAPUnknownEvent))]
+    [JsonDerivedType(typeof(MAPInitLevelEvent), nameof(MAPInitLevelEvent))]
+    [JsonDerivedType(typeof(MAPJSREvent), nameof(MAPJSREvent))]
+    [JsonDerivedType(typeof(MAPLoopEvent), nameof(MAPLoopEvent))]
+    [JsonDerivedType(typeof(MAPObjectEvent), nameof(MAPObjectEvent))]
+    [JsonDerivedType(typeof(MAPPathObjectEvent), nameof(MAPPathObjectEvent))]
+    [JsonDerivedType(typeof(MAPSetBG), nameof(MAPSetBG))]
+    [JsonDerivedType(typeof(MAPWaitEvent), nameof(MAPWaitEvent))]
+#endif
+	public abstract class MAPEvent
+	{
+		/// <summary>
+		/// The time in the level script this event appears at
+		/// </summary>
+		public int LevelDelay { get; set; }
+		public MAPCtrlVars CtrlOptCode = MAPCtrlVars.None;
+		public virtual string EventName { get; set; }
+		[JsonIgnore]
+		public ASMLine Callsite { get; set; }
+		/// <summary>
+		/// Overriden in inheritors -- the list of macros that are compatible with this type of MAPEvent
+		/// </summary>
+		protected abstract string[] CompatibleMacros { get; }
 
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that sets the delay component of a map object
-    /// </summary>
-    public interface IMAPDelayEvent : IMAPEventComponent
-    {
-        int Delay { get; }
-    }
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that sets the spawn location component of a map object
-    /// </summary>
-    public interface IMAPLocationEvent : IMAPEventComponent
-    {
-        int X { get; }
-        int Y { get; }
-        int Z { get; }
-    }
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that sets the shape component of a map object
-    /// </summary>
-    public interface IMAPShapeEvent : IMAPEventComponent
-    {
-        /// <summary>
-        /// The line containing the label before the header is set in the SHAPES.ASM file it belongs in.
-        /// </summary>
-        int ShapeDefinitionLabel { get; }
-        string ShapeName { get; }
-    }
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that sets the strategy component of a map object
-    /// </summary>
-    public interface IMAPStrategyEvent : IMAPEventComponent
-    {
-        string StrategyName { get; }
-    }
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that sets the path component of a map object
-    /// </summary>
-    public interface IMAPPathEvent : IMAPEventComponent
-    {
-        string PathName { get; }
-    }
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that sets the health / attack power of a fighter
-    /// </summary>
-    public interface IMAPHealthAttackEvent : IMAPEventComponent
-    {
-        /// <summary>
-        /// Health Power
-        /// </summary>
-        int HP { get; }
-        /// <summary>
-        /// Attack Power
-        /// </summary>
-        int AP { get; }
-    }
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that has a name
-    /// </summary>
-    public interface IMAPNamedEvent : IMAPEventComponent
-    {
-        /// <summary>
-        /// The name of the this event
-        /// </summary>
-        string Name { get; }
-    }
-    /// <summary>
-    /// Represents a <see cref="MAPEvent"/> that has a name
-    /// </summary>
-    public interface IMAPValueEvent : IMAPEventComponent
-    {
-        /// <summary>
-        /// The name of the this event
-        /// </summary>
-        string Value { get; }
-    }
-    public interface IMAPBGEvent : IMAPEventComponent
-    {
-        /// <summary>
-        /// The name of the background as it appears in code.
-        /// <see cref="MAPSetBG.TranslateNameToMAPContext(in string, string)"/>
-        /// </summary>
-        string Background { get; }
-    }
+		protected MAPEvent()
+		{
+
+		}
+		/// <summary>
+		/// Base constructor for all map events
+		/// </summary>
+		/// <param name="callsite"></param>
+		protected MAPEvent(ASMLine callsite) : this()
+		{
+			Parse(callsite);
+		}
+		/// <summary>
+		/// Checks if the given <see cref="ASMLine"/> contains a macro invocation
+		/// that is compatible with this type of <see cref="MAPEvent"/>
+		/// </summary>
+		/// <param name="Line"></param>
+		/// <returns></returns>
+		internal bool IsCompatible(ASMLine Line)
+		{
+			if (!Line.HasStructureApplied) return false;
+			if (!(Line.Structure is ASMMacroInvokeLineStructure)) return false;
+			var structure = Line.StructureAsMacroInvokeStructure;
+			return CompatibleMacros.Contains(structure.MacroReference.Name.ToLower());
+		}
+		/// <summary>
+		/// Tries to parse a new map event out of the given macro invoke expression
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="Callsite"></param>
+		/// <param name="Result"></param>
+		/// <returns></returns>
+		internal static bool TryParse<T>(ASMLine Callsite, out T Result) where T : MAPEvent, new()
+		{
+			var t = new T();
+			Result = t;
+			if (t.IsCompatible(Callsite)) {
+				t.Parse(Callsite);
+				return true;
+			}
+			return false;
+		}
+		protected abstract void Parse(ASMLine Line);
+		/// <summary>
+		/// Basic <see cref="int.TryParse(string?, out int)"/> or returns <see langword="default"/> (which is 0)
+		/// </summary>
+		/// <param name="content"></param>
+		/// <returns></returns>
+		protected int TryParseOrDefault(string content)
+		{
+			if (string.IsNullOrEmpty(content)) return 0;
+			if (int.TryParse(content, out int result)) { return result; }
+			return 0;
+		}
+	}
+
+	/// <summary>
+	/// Base interface for all MapEventComponents
+	/// <para/><see cref="IMAPDelayEvent"/> etc.
+	/// </summary>
+	public interface IMAPEventComponent { }
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that sets the delay component of a map object
+	/// </summary>
+	public interface IMAPDelayEvent : IMAPEventComponent
+	{
+		int Delay { get; }
+	}
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that sets the spawn location component of a map object
+	/// </summary>
+	public interface IMAPLocationEvent : IMAPEventComponent
+	{
+		int X { get; }
+		int Y { get; }
+		int Z { get; }
+	}
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that sets the shape component of a map object
+	/// </summary>
+	public interface IMAPShapeEvent : IMAPEventComponent
+	{
+		/// <summary>
+		/// The line containing the label before the header is set in the SHAPES.ASM file it belongs in.
+		/// </summary>
+		int ShapeDefinitionLabel { get; }
+		string ShapeName { get; }
+	}
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that sets the strategy component of a map object
+	/// </summary>
+	public interface IMAPStrategyEvent : IMAPEventComponent
+	{
+		string StrategyName { get; }
+	}
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that sets the path component of a map object
+	/// </summary>
+	public interface IMAPPathEvent : IMAPEventComponent
+	{
+		string PathName { get; }
+	}
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that sets the health / attack power of a fighter
+	/// </summary>
+	public interface IMAPHealthAttackEvent : IMAPEventComponent
+	{
+		/// <summary>
+		/// Health Power
+		/// </summary>
+		int HP { get; }
+		/// <summary>
+		/// Attack Power
+		/// </summary>
+		int AP { get; }
+	}
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that has a name
+	/// </summary>
+	public interface IMAPNamedEvent : IMAPEventComponent
+	{
+		/// <summary>
+		/// The name of the event
+		/// </summary>
+		string Name { get; }
+	}
+
+	/// <summary>
+	/// Represents a <see cref="MAPEvent"/> that has a name
+	/// </summary>
+	public interface IMAPValueEvent : IMAPEventComponent
+	{
+		/// <summary>
+		/// The name of the event
+		/// </summary>
+		string Value { get; }
+	}
+
+	public interface IMAPBGEvent : IMAPEventComponent
+	{
+		/// <summary>
+		/// The name of the background as it appears in code.
+		/// <see cref="MAPSetBG.TranslateNameToMAPContext(in string, string)"/>
+		/// </summary>
+		string Background { get; }
+	}
 }
--- StarFox.Interop/MAP/MAPData.cs
+++ StarFox.Interop/MAP/MAPData.cs
@@ -9,139 +9,129 @@ using StarFox.Interop.MISC;
 
 namespace StarFox.Interop.MAP
 {
-    /// <summary>
-    /// Represents the data used to create a level in Starfox.
-    /// </summary>
-    public class MAPData
-    {
-        [Serializable] public class MAPRegionContext
-        {
-            /// <param name="labelName"> The name of the inline label for this region </param>
-            /// <param name="asmChunkIndex"> The index of chunk the Inline Label appears at </param>
-            /// <param name="estimatedTimeStart"> The estimated LevelTime (Delay) this might appear at. This is useful
-            /// for a visual editor showing where this loop might be.</param>
-            public MAPRegionContext(string labelName, uint asmChunkIndex, int estimatedTimeStart)
-            {
-                LabelName          = labelName;
-                ASMChunkIndex      = asmChunkIndex;
-                EstimatedTimeStart = estimatedTimeStart;
-            }
+	/// <summary>
+	/// Represents the data used to create a level in Starfox.
+	/// </summary>
+	public class MAPData
+	{
+		[Serializable] public class MAPRegionContext
+		{
+			/// <param name="labelName"> The name of the inline label for this region </param>
+			/// <param name="asmChunkIndex"> The index of chunk the Inline Label appears at </param>
+			/// <param name="estimatedTimeStart"> The estimated LevelTime (Delay) this might appear at. This is useful
+			/// for a visual editor showing where this loop might be.</param>
+			public MAPRegionContext(string labelName, uint asmChunkIndex, int estimatedTimeStart)
+			{
+				LabelName = labelName;
+				ASMChunkIndex = asmChunkIndex;
+				EstimatedTimeStart = estimatedTimeStart;
+			}
 
-            public string LabelName { get; private set; }
-            public uint ASMChunkIndex { get; private set; }
-            public int EstimatedTimeStart { get; private set; }
+			public string LabelName { get; private set; }
+			public uint ASMChunkIndex { get; private set; }
+			public int EstimatedTimeStart { get; private set; }
 
-            /// <summary>
-            /// The estimated LevelTime (Delay) this might end at. This is useful
-            /// for a visual editor showing where this loop might be
-            /// </summary>
-            public int EstimatedTimeEnd { get; set; }
-            /// <summary>
-            /// If there are any maploops that use this label
-            /// </summary>
-            public bool IsLooped => ReferencedLoops.Any();
-            /// <summary>
-            /// Any <see cref="MAPLoopEvent"/> that references this section/region
-            /// <para/> For all intents and purposes this should only ever have one item or none at all.
-            /// Not sure if multiple <c>maploop</c>s using the same spot is even feasible
-            /// </summary>
-            public HashSet<MAPLoopEvent> ReferencedLoops { get; set; } = new HashSet<MAPLoopEvent>();
-        }
+			/// <summary>
+			/// The estimated LevelTime (Delay) this might end at. This is useful
+			/// for a visual editor showing where this loop might be
+			/// </summary>
+			public int EstimatedTimeEnd { get; set; }
+			/// <summary>
+			/// If there are any map loops that use this label
+			/// </summary>
+			public bool IsLooped => ReferencedLoops.Any();
+			/// <summary>
+			/// Any <see cref="MAPLoopEvent"/> that references this section/region
+			/// <para/> For all intents and purposes this should only ever have one item or none at all.
+			/// Not sure if multiple <c>maploop</c>s using the same spot is even feasible
+			/// </summary>
+			public HashSet<MAPLoopEvent> ReferencedLoops { get; set; } = new HashSet<MAPLoopEvent>();
+		}
 
-        /// <summary>
-        /// The events that make up this level script
-        /// <para>This is also sometimes referred to as a ZDepth Table</para>
-        /// </summary>
-        public HashSet<MAPEvent> Events { get; set; } = new HashSet<MAPEvent>();
-        /// <summary>
-        /// Get only events that have attached shape data
-        /// </summary>
-        public IEnumerable<IMAPShapeEvent> ShapeEvents => Events.OfType<IMAPShapeEvent>();
-        /// <summary>
-        /// All of the events of this MAPScript, in order, with accompanying DELAY calculated based on the previous events.
-        /// <para>KEY is the index of the event in the <see cref="Events"/> property.</para>
-        /// </summary>
-        public Dictionary<int, int> EventsByDelay { get; set; } = new Dictionary<int, int>();
-        /// <summary>
-        /// Maps have labels that can be used in loops to create repeated sections of levels without
-        /// copy/pasting, for example.
-        /// <para/>This details where those occur in the base <see cref="ASMFile"/> this map was imported from
-        /// </summary>
-        public Dictionary<string, MAPRegionContext> SectionMarkers { get; } = new Dictionary<string, MAPRegionContext>();
+		/// <summary>
+		/// The events that make up this level script
+		/// <para>This is also sometimes referred to as a ZDepth Table</para>
+		/// </summary>
+		public HashSet<MAPEvent> Events { get; set; } = new HashSet<MAPEvent>();
+		/// <summary>
+		/// Get only events that have attached shape data
+		/// </summary>
+		public IEnumerable<IMAPShapeEvent> ShapeEvents => Events.OfType<IMAPShapeEvent>();
+		/// <summary>
+		/// All the events of this MAPScript, in order, with accompanying DELAY calculated based on the previous events.
+		/// <para>KEY is the index of the event in the <see cref="Events"/> property.</para>
+		/// </summary>
+		public Dictionary<int, int> EventsByDelay { get; set; } = new Dictionary<int, int>();
+		/// <summary>
+		/// Maps have labels that can be used in loops to create repeated sections of levels without
+		/// copy/pasting, for example.
+		/// <para/>This details where those occur in the base <see cref="ASMFile"/> this map was imported from
+		/// </summary>
+		public Dictionary<string, MAPRegionContext> SectionMarkers { get; } = new Dictionary<string, MAPRegionContext>();
 
-        /// <summary>
-        /// Merges all the events into one *new* MAPData instance, keeps context data from Parent map
-        /// </summary>
-        /// <param name="ParentMap"></param>
-        /// <param name="MergeChild"></param>
-        /// <returns></returns>
-        public static MAPData Combine(MAPData ParentMap, MAPData MergeChild)
-        {
-            MAPData newMap = new MAPData()
-            {
+		/// <summary>
+		/// Merges all the events into one *new* MAPData instance, keeps context data from Parent map
+		/// </summary>
+		/// <param name="ParentMap"></param>
+		/// <param name="MergeChild"></param>
+		/// <returns></returns>
+		public static MAPData Combine(MAPData ParentMap, MAPData MergeChild)
+		{
+			MAPData newMap = new MAPData()
+			{
 
-            };
-            if (ParentMap.Events.Count != ParentMap.EventsByDelay.Count)
-                throw new InvalidDataException("PARENT Events and EventsByDelay are not the same!!!");
-            if (MergeChild.Events.Count != MergeChild.EventsByDelay.Count)
-                throw new InvalidDataException("CHILD Events and EventsByDelay are not the same!!!");
-            int runningIndex = 0;
-            for (int i = 0; i < ParentMap.Events.Count; i++)
-            {
-                var evt = ParentMap.Events.ElementAt(i);
-                newMap.Events.Add(evt);
-                var dlyevt = ParentMap.EventsByDelay.ElementAt(i);
-                newMap.EventsByDelay.Add(runningIndex + dlyevt.Key, dlyevt.Value);
-            }
-            runningIndex = newMap.Events.Count;
-            for (int i = 0; i < MergeChild.Events.Count; i++)
-            {
-                var evt = MergeChild.Events.ElementAt(i);
-                newMap.Events.Add(evt);
-                var dlyevt = MergeChild.EventsByDelay.ElementAt(i);
-                newMap.EventsByDelay.Add(runningIndex + dlyevt.Key, dlyevt.Value);
-            }
-            return newMap;
-        }
+			};
+			if (ParentMap.Events.Count != ParentMap.EventsByDelay.Count)
+				throw new InvalidDataException("PARENT Events and EventsByDelay are not the same!!!");
+			if (MergeChild.Events.Count != MergeChild.EventsByDelay.Count)
+				throw new InvalidDataException("CHILD Events and EventsByDelay are not the same!!!");
+			int runningIndex = 0;
+			for (int i = 0; i < ParentMap.Events.Count; i++) {
+				var evt = ParentMap.Events.ElementAt(i);
+				newMap.Events.Add(evt);
+				var dlyevt = ParentMap.EventsByDelay.ElementAt(i);
+				newMap.EventsByDelay.Add(runningIndex + dlyevt.Key, dlyevt.Value);
+			}
+			runningIndex = newMap.Events.Count;
+			for (int i = 0; i < MergeChild.Events.Count; i++) {
+				var evt = MergeChild.Events.ElementAt(i);
+				newMap.Events.Add(evt);
+				var dlyevt = MergeChild.EventsByDelay.ElementAt(i);
+				newMap.EventsByDelay.Add(runningIndex + dlyevt.Key, dlyevt.Value);
+			}
+			return newMap;
+		}
 
-        [Serializable]
-        private class Intermediary
-        {
-            public string Title { get; set; }
-            public Dictionary<int, int> EventsByDelay { get; set; } = new Dictionary<int, int>();
-            public byte[] SerializedData { get; set; }
-        }
+		[Serializable]
+		private class Intermediary
+		{
+			public Dictionary<int, int> EventsByDelay { get; set; } = new Dictionary<int, int>();
+			public List<MAPEvent> MapEvents { get; set; } = new List<MAPEvent>();
+		}
 
 		/// <summary>
 		/// Serializes this object to the given stream
 		/// </summary>
-		/// <param name="Destination"></param>
-        public void Serialize(Stream destination)
+		/// <param name="destination"></param>
+		public void Serialize(Stream destination)
 		{
-			using (MemoryStream mem = new MemoryStream()) {
-                StrongTypeSerialization.SerializeObjects(mem, Events);
-                var inter = new Intermediary() {
-                    EventsByDelay = EventsByDelay,
-                    SerializedData = mem.ToArray()
-                };
-                JsonImportExport.Serialize(inter, destination);
-            }
-        }
+			var inter = new Intermediary()
+			{
+				EventsByDelay = EventsByDelay,
+				MapEvents = Events.ToList()
+			};
+			JsonImportExport.Serialize(inter, destination);
+		}
 
-        public static async Task<MAPData> Deserialize(Stream Json)
-        {
-	        var inter = await JsonImportExport.LoadTo<Intermediary>(Json);
+		public static async Task<MAPData> Deserialize(Stream Json)
+		{
+			var inter = await JsonImportExport.LoadTo<Intermediary>(Json);
 			if (inter == null) throw new Exception("Couldn't create the intermediary!");
-            var data = new MAPData()
-            {
-                EventsByDelay = inter.EventsByDelay,
-            };
-            using (MemoryStream stream = new MemoryStream(inter.SerializedData))
-            {
-                var evts = await StrongTypeSerialization.DeserializeObjects(stream);
-                data.Events = new HashSet<MAPEvent>(evts.Cast<MAPEvent>());
-            }
-            return data;
-        }
-    }
+			return new MAPData()
+			{
+				EventsByDelay = inter.EventsByDelay,
+				Events = new HashSet<MAPEvent>(inter.MapEvents)
+			};
+		}
+	}
 }
